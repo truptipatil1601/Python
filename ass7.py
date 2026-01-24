a2094b4d063424e63b5a0365b20dd3166dfe62e6
Trupti Sagar Patil <truptip1601@gmail.com>
2026-01-24 23:05:34 +0530

    Add files via upload

---
+++ ass7.py
@@ -0,0 +1,9 @@
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+# Function calls
+print(is_even(8))
+print(is_even(9))
---
+++ ass8.py
@@ -0,0 +1,11 @@
+# Define two lists
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+
+# Function to concatenate two lists
+def concatenate_lists(l1, l2):
+    return l1 + l2
+
+# Function call and print result
+result = concatenate_lists(list1, list2)
+print(result)
